53bfbf4ef0b53748dd55246489ab895202241d30
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-07-26 23:32:42 -0300

    Router(MODEL) de Restaurantes

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 import {Server} from './server/server' //Importando o servidor criado
 import {usersRouter} from './users/users.router'
+import {restaurantsRouter} from './restaurants/restaurants.router'
 
 const server = new Server()
-server.bootstrap([usersRouter]).then(server => { //Conectado com sucesso
+server.bootstrap([
+  usersRouter,
+  restaurantsRouter
+]).then(server => { //Conectado com sucesso
   console.log('Server is listening on: ', server.application.address())
 }).catch(err => {
   console.log('Server failed to start')
