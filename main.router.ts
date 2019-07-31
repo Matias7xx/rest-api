b2a214bbe3da35b8a59b566b5554ea8db4019fd8
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-07-30 21:52:31 -0300

    Criando rota MAIN com o link de todas as rotas

---
+++ main.router.ts
@@ -0,0 +1,17 @@
+//ROTA PRINCIPAL DA APLICAÇÃO (MAIN)
+import { Router } from './common/router'
+import * as restify from 'restify'
+
+class MainRouter extends Router {
+    applyRoutes(application: restify.Server) {
+        application.get('/', (req, resp, next) => {
+            resp.json({
+                users: '/users',
+                restaurants: '/restaurants',
+                reviews: '/reviews'
+            })
+        })
+    }
+}
+
+export const mainRouter = new MainRouter()
