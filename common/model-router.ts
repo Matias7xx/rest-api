f835dedcb9f6c497b04c3fc87dd82e9a8524f365
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-07-30 23:39:17 -0300

    PAGINAÇÃO refatorando count depreciado

--- common/model-router.ts
+++ common/model-router.ts
@@ -61,7 +61,7 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
         const skip = (page -1) * this.pageSize
 
         this.model
-            .count({}).exec()
+            .countDocuments({}).exec()
             .then(count => this.model.find()
             .skip(skip)
             .limit(this.pageSize) //Paginação
