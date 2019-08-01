23fd555c1afa4d88829485b36c35f754d2f90fc9
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-07-31 21:17:49 -0300

    Refactoring

--- users/users.router.ts
+++ users/users.router.ts
@@ -26,14 +26,11 @@ class UsersRouter extends ModelRouter<User> {
     findByEmail = (req, res, next) => {
         if(req.query.email) {
             User.findByEmail(req.query.email)
-                .then(user => {
-                    if(user) {
-                        return [user]
-                    } else {
-                        return []
-                    }
-                })
-                .then(this.renderAll(res, next))
+                .then(user => user ? [user] : [])
+                .then(this.renderAll(res, next, {
+                    pageSize: this.pageSize,
+                    url: req.url
+                }))
                 .catch(next)
         } else {
             next()
