c9a78a2381888f9925bac8f77170830bd32ac5b4
Gabriel Nóbrega <matiasnobrega7@gmail.com>
2019-07-31 21:05:55 -0300

    TESTE GETBYID E PATCH (CONFLITO)

--- users/users.test.ts
+++ users/users.test.ts
@@ -35,7 +35,7 @@ test('post /users', () => {
         .post('/users')
         .send({
             name: 'UsuarioTeste',
-            email: 'teste@email.com',
+            email: 'testepost@email.com',
             password: '1234567',
             cpf: '121.627.234-41'
         })
@@ -43,10 +43,41 @@ test('post /users', () => {
             expect(response.status).toBe(200)
             expect(response.body._id).toBeDefined()
             expect(response.body.name).toBe('UsuarioTeste')
-            expect(response.body.email).toBe('teste@email.com')
+            expect(response.body.email).toBe('testepost@email.com')
             expect(response.body.cpf).toBe('121.627.234-41')
             expect(response.body.password).toBeUndefined()
         }).catch(fail)
+})
+
+test('get /users/aaaaa - not found', () => {
+    return request(address)
+        .get('/users/aaaaa')
+        .then(response => {
+            expect(response.status).toBe(404) //Espero que a requisição seja um sucesso
+        }).catch(fail)
+})
+
+test('patch /users/:id',() => {
+    return request(address)
+        .post('/users')
+        .send({
+            name: 'UsuarioTestePatch',
+            email: 'testepatch2@email.com',
+            password: '1234567',
+        })
+        .then(response => request(address)
+                            .patch(`/users/${response.body._id}`)
+                            .send({
+                                name: 'usuario2 - atualizado'
+                            }))
+        .then(response => {
+            expect(response.status).toBe(200)
+            expect(response.body._id).toBeDefined()
+            expect(response.body.name).toBe('usuario2 - atualizado')
+            expect(response.body.email).toBe('testepatch2@email.com')
+            expect(response.body.password).toBeUndefined()
+        })
+        .catch(fail)
 })
 
 //yarn test para executar
